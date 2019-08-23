7edcd435a7135270005dc2462d2f61a6a5ef928a
Valery Briz <valerybriz@gmail.com>
2019-08-23 18:32:41 -0500

    add comments to the main script

--- breakout_detection_wraper/__main__.py
+++ breakout_detection_wraper/__main__.py
@@ -7,19 +7,24 @@ from rpy2.robjects.vectors import IntVector, FloatVector
 
 def main():
     mydata = []
-
+    # Open and Read the data file from a csv to convert it to a list
     with open('breakout_detection_wraper/fuel_data.csv', 'r') as csvfile:
         dat = csv.reader(csvfile)
         for line in dat:
             mydata.append(float(line[0]))
 
+    # Define the parameters to configure the break out detection algoritm
     minsize = 30
     method = 'multi'
     degree = 1
+    # Open the R file to run it on the wrapper
     with open('breakout_detection_wraper/breakout_function.R') as code:
         rcode = os.linesep.join(code.readlines())
+        # Create the wrapper as an anonymous package signature
         wrapper = SignatureTranslatedAnonymousPackage(rcode, "Codigo")
+    # Execute the method from the wrapper
     result = wrapper.Detect(FloatVector(mydata), minsize, method, degree)
+    # Print the result returned from the R function
     print(result)
 
 
